e20fc06645a374a560805ce86bf4462da9b77c56
Ryanrob327 <ryanrob327@gmail.com>
2026-05-13 11:44:28 -0700

    initialize main.cpp and delete ball

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,34 @@
 #include <raylib.h>
-#include "ball.h"
+
+
 
 int main() 
 {
     const Color darkGreen = {20, 160, 133, 255};
     
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
+    constexpr int screenWidth = 1600;
+    constexpr int screenHeight = 900;
     
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+
+    InitWindow(screenWidth, screenHeight, "Orbital Physics Simulation");
     SetTargetFPS(60);
     
+
     while (!WindowShouldClose())
     {
-        ball.Update();
+        // Get delta time
+        float deltaTime = GetFrameTime();
+
+        // Updating
+        
+
+        // Collision
+  
         
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+            // Drawing
+            ClearBackground(BLACK);
+            
         EndDrawing();
     }
     
