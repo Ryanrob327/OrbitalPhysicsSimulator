0bf319e07f1d907bca6f540cfbbc4c0877a266d1
Ryanrob327 <ryanrob327@gmail.com>
2026-05-13 11:42:17 -0700

    initialized celestial body class

---
+++ src/celestial-bodies/celestialBody.cpp
@@ -0,0 +1,39 @@
+#include "celestialBody.h"
+#include <raylib.h>
+
+
+
+CelestialBody::CelestialBody(
+    Vector2 position,
+    Vector2 velocity,
+    float mass,
+    float radius,
+    Color color
+)
+{
+    position = position;
+    velocity = velocity;
+    acceleration = {0.0f, 0.0f};
+    mass = mass;
+    radius = radius;
+    color = color;
+}
+
+// Update physics
+void CelestialBody::Update(float deltaTime)
+{
+    // Velocity changes from acceleration
+    velocity.x += acceleration.x * deltaTime;
+    velocity.y += acceleration.y * deltaTime;
+
+    // Position changes from velocity
+    position.x += velocity.x * deltaTime;
+    position.y += velocity.y * deltaTime;
+
+    // Reset acceleration every frame
+    acceleration = { 0.0f, 0.0f };
+}
+
+void CelestialBody::Draw(){
+    DrawCircle(position.x, position.y, radius, GREEN);
+}
---
+++ src/celestial-bodies/celestialBody.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+
+class CelestialBody{
+    public:
+        CelestialBody(
+            Vector2 position,
+            Vector2 velocity,
+            float mass,
+            float radius,
+            Color color
+        );
+        void Draw();
+        void Update(float deltaTime);
+
+        Vector2 position;
+        Vector2 velocity;
+        Vector2 acceleration;
+        float mass;
+        float radius;
+        Color color;
+};
