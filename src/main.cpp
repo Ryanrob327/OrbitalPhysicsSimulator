70d751ce52d79cd245603b835efe77b35b2f085f
Ryanrob327 <ryanrob327@gmail.com>
2026-05-13 20:44:58 -0700

    camera

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,23 @@
 
 
 
+void UpdateCamera(Camera2D& camera, float delta){
+    float cameraSpeed = 500.0f;
+    Vector2 input = {0, 0};
+    if (IsKeyDown(KEY_RIGHT)) input.x += 1; // camera movement control
+    if (IsKeyDown(KEY_LEFT))  input.x -= 1;
+    if (IsKeyDown(KEY_DOWN))  input.y += 1;
+    if (IsKeyDown(KEY_UP))    input.y -= 1;
+
+    camera.target.x += input.x * cameraSpeed * delta; // smooth camera movement
+    camera.target.y += input.y * cameraSpeed * delta;
+
+    camera.zoom += GetMouseWheelMove() * 0.1f; // camera scroll control
+    if (camera.zoom < 0.2f) camera.zoom = 0.2f;
+    if (camera.zoom > 5.0f) camera.zoom = 5.0f;
+}
+
+
 int main() 
 {
     const Color darkGreen = {20, 160, 133, 255};
@@ -18,8 +35,15 @@ int main()
     CelestialBody moon({600,450}, {0,-32}, 500, 5, WHITE);
     PhysicsSimulator simulator;
 
+    // Camera
+    Camera2D camera = {0};
+    camera.target = {800, 450};
+    camera.offset = {1600/2.0f, 900/2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
-    while (!WindowShouldClose())
+
+    while (!WindowShouldClose()) // Main Game Loop
     {
         // Get delta time
         float delta = GetFrameTime();
@@ -31,12 +55,18 @@ int main()
 
         // Collision
 
+
+        // Camera
+        UpdateCamera(camera, delta);
+
         
         BeginDrawing();
             // Drawing
             ClearBackground(BLACK);
-            earth.Draw();
-            moon.Draw();
+            BeginMode2D(camera);
+                earth.Draw();
+                moon.Draw();
+            EndMode2D();
         EndDrawing();
     }
     
